3281893e295f7f2f3a59add31e9234b950ab8ec5
masato <thangloitran406@gmail.com>
2024-05-09 23:25:59 +0700

    fix: pull newest code from main branch

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Main } from './pages/admin/Layout/Main;
+import { Main } from './pages/admin/Layout/Main';
 import { Login } from './pages/admin/Login/Login';
 import MainGuest from './pages/guest/main';
 import { Register } from './pages/admin/Login/Register';
@@ -8,13 +8,13 @@ import { Register } from './pages/admin/Login/Register';
 function App() {
     return (
         <div className="App">
+          {/* Sau này chỗ này cần tạo router và layout để quản lý việc chuyển trang */}
           {/* <Header/>
           <main>
             <Homepage />
           </main> */}
-          {/* Sau này chỗ này cần tạo router và layout để quản lý việc chuyển trang */}
+          {/* <MainGuest /> */}
           <Main>
-            {/*<MainGuest />*/}
             <Register />
           </Main>
         </div>
